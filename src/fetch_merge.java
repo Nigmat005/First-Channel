ece2f4ec11d38733f49ae785dd8916306eb9eedb
Nigmat005 <Nigmathalmamat@gmail.com>
2020-08-01 17:18:38 -0400

    Example-for-fetch- and-merge.java

--- src/fetch_merge.java
+++ src/fetch_merge.java
@@ -2,5 +2,6 @@ public class fetch_merge {
     public static void main(String[] args) {
         System.out.println("Example for git fetch and merge");
         System.out.println("git pull=git fetch +git merge");
+        //??
     }
 }
