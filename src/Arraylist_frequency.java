1831779ba6d591e8a883fc77cc3279d43e6af935
Nigmat005 <Nigmathalmamat@gmail.com>
2020-08-02 14:13:00 -0400

    Remove_and _get_count_using_Arraylist

--- src/Arraylist_frequency.java
+++ src/Arraylist_frequency.java
@@ -1,23 +1,29 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 
 public class Arraylist_frequency{
     public static void main(String[] args) {
         // I wanna know all frequency of single word showed up in string.
-    String input="AAABBBCCDD";
+    String input="AAABBBCCDD";String res="";
         ArrayList<String>list=new ArrayList<>();
     for(int i=0;i<input.length();i++){
         // Amending begins below
-        if(!list.contains(Arrays.asList(input.charAt(i))){
-         list.add(Character.toString(input.charAt(i)));
+        if(!res.contains(input.charAt(i)+"")){
+            res+=input.charAt(i);
         }
         //list.add(Character.toString(input.charAt(i)));
     }
+//    for(String each1:res.split("")){
+//        list.add(each1);
+//    }
+        list.addAll(Arrays.asList(input.split("")));
         System.out.println(list);
-for(String each:list){
-    System.out.println(each+Collections.frequency(list,each));
+
+for(String each:res.split(""))// here if you wanna use for-each loop you need to use non-primitive data, meaning use valued data{
+    System.out.print(each+Collections.frequency(list,each));
 }
 
 
@@ -25,4 +31,4 @@ for(String each:list){
     }
 
 
-}
+
